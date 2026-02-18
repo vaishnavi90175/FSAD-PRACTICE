334e1c158bca259228df6f344bb45ece32142d8c
vaishnavi90175 <2400090175@kluniversity.in>
2026-02-18 09:36:47 +0530

    commited

---
+++ SpringBootDemoApp/src/main/java/com/klu/SpringBootDemoApp.java
@@ -0,0 +1,12 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApp.class, args);
+    }
+}
